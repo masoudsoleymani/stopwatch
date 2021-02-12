653cd0cbcfab9bb11a74d53037ba28784b398c13
Masoud Soleymani <masoud.soleymani@daimler.com>
2021-02-12 22:40:30 +0000

    build docs folder

---
+++ docs/precache-manifest.0dcd62d6645bf1e2f27ade1de852756d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4491596121095a0c692c5cb3d1188b9",
+    "url": "/stopwatch/index.html"
+  },
+  {
+    "revision": "1252f44a84f0b066e937",
+    "url": "/stopwatch/static/css/main.38cbfd11.chunk.css"
+  },
+  {
+    "revision": "549f6620eb736c62b30b",
+    "url": "/stopwatch/static/js/2.0ee04247.chunk.js"
+  },
+  {
+    "revision": "1252f44a84f0b066e937",
+    "url": "/stopwatch/static/js/main.6c10b32b.chunk.js"
+  },
+  {
+    "revision": "8e966cc4b8908ce6b302",
+    "url": "/stopwatch/static/js/runtime~main.776e2c8f.js"
+  }
+]);
